a7b7f42cf4c17b203d583d28ef3ea93053a6c85b
Yusef-H <yusefhalabi456@gmail.com>
2023-04-16 08:59:23 +0300

    add menu module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './style.css';
 import initialize from './init.js';
-
+import displayHome from './home.js';
+import displayMenu from './menu.js';
 initialize();
+// displayHome();
+displayMenu();
+
+
+
 
---
+++ src/menu.js
@@ -0,0 +1,38 @@
+import tomaHawk from './images/tomahawk.jpg';
+import fillet from './images/fillet.jpg';
+
+const menuBoard = document.createElement('div');
+const content = document.querySelector('.content');
+function displayMenu(){    
+    menuBoard.classList.add('menu-board');
+
+    const title = document.createElement('div');
+    title.innerHTML = "Menu";
+    title.classList.add('home-title');
+    menuBoard.appendChild(title);
+
+    addMenuItem(tomaHawk);
+    addMenuItem(fillet);
+
+    content.appendChild(menuBoard);
+
+    return menuBoard;
+}
+
+function addMenuItem(tomaHawk){
+    const firstItem = document.createElement('div');
+    firstItem.classList.add('menu-item');
+    const firstItemImage = new Image();
+    firstItemImage.src = tomaHawk;
+    firstItemImage.classList.add('image');
+    firstItem.appendChild(firstItemImage);
+    const firstItemInfo = document.createElement('div');
+    firstItemInfo.innerHTML = 'Juicy Tomahawk Steak';
+    firstItemInfo.classList.add('item-info');
+    firstItem.appendChild(firstItemInfo);
+    menuBoard.append(firstItem);
+}
+
+
+
+export default displayMenu;
