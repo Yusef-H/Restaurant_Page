7120f57597eb55bbace06cbe970c89052d5caaa8
Yusef-H <yusefhalabi456@gmail.com>
2023-04-16 09:56:37 +0300

    fix styles

--- src/menu.js
+++ src/menu.js
@@ -1,25 +1,25 @@
 import tomaHawk from './images/tomahawk.jpg';
 import fillet from './images/fillet.jpg';
 
-const menuBoard = document.createElement('div');
 const content = document.querySelector('.content');
 
 function displayMenu(){    
+    const menuBoard = document.createElement('div');
     menuBoard.classList.add('menu-board');
     const title = document.createElement('div');
     title.innerHTML = "Menu";
     title.classList.add('home-title');
     menuBoard.appendChild(title);
 
-    addMenuItem(tomaHawk);
-    addMenuItem(fillet);
+    addMenuItem(tomaHawk, menuBoard);
+    addMenuItem(fillet, menuBoard);
 
     content.appendChild(menuBoard);
 
     return menuBoard;
 }
 
-function addMenuItem(tomaHawk){
+function addMenuItem(tomaHawk, menuBoard){
     const firstItem = document.createElement('div');
     firstItem.classList.add('menu-item');
     const firstItemImage = new Image();
